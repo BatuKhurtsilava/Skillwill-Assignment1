50cbada31909f8601dfd77edcc467de6880a9fb6
ThinkPad <khurtsilavabatu1@gmail.com>
2023-11-29 15:37:17 +0400

    index and script added

---
+++ script.js
@@ -0,0 +1,42 @@
+
+
+
+// function sayMyName(name) {
+// console.log(name)
+// }
+
+// const sayMyName = function(name) {
+//   console.log(name)
+//   }
+
+
+
+
+  const sayMyName =  () => {
+    if(true)  {
+      let friendName = 'luka'
+      console.log(friendName)
+  }
+    console.log(friendName)
+    }
+
+    sayMyName();
+
+   
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
